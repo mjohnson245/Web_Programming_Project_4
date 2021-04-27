ba908602c3cb37f24218af71def49fd3fc986426
Matthew Johnson <43484727+mjohnson245@users.noreply.github.com>
2021-04-27 11:15:13 -0400

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Car Rental Checkout</title>
+</head>
+<body>
+	<h1>Checkout</h1>
+</body>
+</html>
---
+++ viewcart.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Your Cart</title>
+</head>
+<body>
+	<h1>Your Cart Below</h1>
+	<div>Show cars here.</div>
+</body>
+</html>
